eb3c5057d7a3f76d722831190c217bb2437b87e2
Dotun Olusoga <adedotun.olusoga@gmail.com>
2015-02-23 15:31:18 -0600

    load grunt task, jade, clean plugins

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,19 +2,24 @@
 
 module.exports = function(grunt) {
 
-  grunt.loadNpmTasks('grunt-contrib-copy');
+  require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
-
+    clean: ['public'],
     copy: {
       main: {
         files: [
-          {expand: true, cwd: 'app/', src: ['**'], dest: 'public/', filter: 'isFile'}
+          {expand: true, cwd: 'app/', src: ['**', '!**/*.jade'], dest: 'public/', filter: 'isFile'}
         ]
       }
+    },
+    jade: {
+      compile: {
+        files: [{expand: true, cwd: 'app/' , src : ['**/*.jade', '!**/_*.jade'], dest: 'public/', ext: '.html'}]
+      }
     }
-
   });
 
   grunt.registerTask('default', []);
+  grunt.registerTask('build', ['clean', 'copy', 'jade']);
 };
